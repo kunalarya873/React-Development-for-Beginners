66f88b04e6e227a9680792fc3499dace6d22e84f
kunalarya873 <kunalarya873@gmail.com>
2024-04-03 13:42:08 +0530

    Adding Classes

--- monsters-rolodex/src/App.js
+++ monsters-rolodex/src/App.js
@@ -3,13 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component{
+  constructor(){
+    super();
+    this.state = {
+      name:  'Andrei',
+    }
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            Hi {this.state.name}
           </p>
           <a
             className="App-link"
